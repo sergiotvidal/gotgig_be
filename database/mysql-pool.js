ed92a905fcd06d74ad6ccdb5eaf4d17876ccc010
sergiotvidal <sergiotvidal2@gmail.com>
2019-06-02 01:33:03 +0200

    setting up index file and database connection defined

---
+++ database/mysql-pool.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const mysql = require('mysql2');
+
+async function connect() {
+  const options = {
+    connectionLimit: 10,
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT,
+    timezone: 'Z',
+    multipleStatements: true,
+  };
+
+  /**
+   * Create connection pool and
+   * promisify it to use async / await
+   */
+  const pool = mysql.createPool(options);
+  this.pool = pool.promise();
+
+  try {
+    const connection = await this.pool.getConnection();
+    if (connection) {
+      connection.release();
+    }
+  } catch (e) {
+    console.error('mysql pool connect', e);
+    throw e;
+  }
+}
+
+async function getConnection() {
+  if (this.pool === null) {
+    throw new Error("MySQL connection didn't established. You must connect first.");
+  }
+
+  const connection = await this.pool.getConnection();
+
+  return connection;
+}
+
+module.exports = {
+  connect,
+  getConnection,
+};
--- index.js
+++ index.js
@@ -5,13 +5,63 @@
  */
 require('dotenv').config();
 
+const bodyParser = require('body-parser');
 const express = require('express');
+const mysqlPool = require('./database/mysql-pool');
 
 const app = express();
+app.use(bodyParser.json());
 
+/**
+ *  CORS configuration
+ */
+
+app.use((req, res, next) => {
+  const accessControlAllowMethods = [
+    'GET',
+    'POST',
+    'DELETE',
+    'HEAD',
+    'PATCH',
+    'PUT',
+    'OPTIONS',
+  ];
+
+  const accessControlAllowHeaders = [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'Accept-Version',
+    'Authorization',
+    'Location',
+  ];
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', accessControlAllowMethods.join(','));
+  res.header('Access-Control-Allow-Headers', accessControlAllowHeaders.join(','));
+  res.header('Access-Control-Expose-Headers', accessControlAllowHeaders.join(','));
+  next();
+});
+
+/**
+ *  Port variable configured for deploy
+ */
 
 const port = process.env.PORT;
 
-app.listen(port, function () {
-  console.log(`App listening on ${port}!`);
-});
+async function init() {
+  try {
+    await mysqlPool.connect();
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+
+  app.listen(port, () => {
+    console.log(`App listening on ${port}!`);
+  });
+}
+
+init();
