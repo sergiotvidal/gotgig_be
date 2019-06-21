b329d479ec91581e9e03f4db8ca667e21e0b6df3
sergiotvidal <sergiotvidal2@gmail.com>
2019-06-21 05:13:24 +0200

    deploy changes

--- webserver/controllers/concerthall/delete-concerthall.js
+++ webserver/controllers/concerthall/delete-concerthall.js
@@ -5,8 +5,10 @@ const mySqlPool = require('../../../database/mysql-pool');
 async function deleteConcerthall(req, res) {
   const idConcerthall = req.params.id_concerthall;
   const connection = await mySqlPool.getConnection();
+  const deleteConcertsQuery = `DELETE FROM concerts WHERE id_localhall = '${idConcerthall}'`;
   const deleteConcerthallQuery = `DELETE FROM localhalls WHERE id_localhall = '${idConcerthall}'`;
   try {
+    await connection.query(deleteConcertsQuery);
     await connection.query(deleteConcerthallQuery);
     connection.release();
     return res.status(200).redirect('/user');
